cf0637da00f7fbd01e7849c8296771101c467bf8
Hemant Gabra <hemant.gabra@teleperformance.com>
2021-01-31 21:27:56 +0530

    play with props and emit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Testing v-if="demo" @showTesting="showTesting($event)"></Testing>
+    <!-- <button @click="showTesting">Click</button> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Testing from "./components/Testing.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Testing
+  },
+  data() {
+    return {
+      demo: false
+    };
+  },
+  methods: {
+    showTesting() {
+      this.demo = true;
+    }
   }
 };
 </script>
---
+++ src/components/Testing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Testing",
+  data() {
+    return {
+      name: "hemant"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
